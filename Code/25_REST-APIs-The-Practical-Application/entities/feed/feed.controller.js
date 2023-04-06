c9ebbbf4edeadc76c44c49ca333fd3e9b34d82bf
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2023-04-06 04:53:32 +0200

    update and delete post

--- Code/25_REST-APIs-The-Practical-Application/entities/feed/feed.controller.js
+++ Code/25_REST-APIs-The-Practical-Application/entities/feed/feed.controller.js
@@ -1,5 +1,9 @@
 import {getValidationErrors} from '../../utils/validation-errors.js';
 import Post from '../post/post.model.js';
+import {deleteFile} from "../../utils/file.js";
+import path from "path";
+import rootDir from "../../utils/path.js";
+
 const getPosts = async (req, res, next) => {
     try {
         const posts = await Post.find();
@@ -53,4 +57,57 @@ const getPost = async (req, res, next) => {
     }
 };
 
-export {getPosts, createPost, getPost};
+const updatePost = async (req, res, next) => {
+    const {postId} = req.params;
+    const {invalid, errors} = getValidationErrors(req);
+    let {imageUrl} = req.body;
+    if (req.file) imageUrl = req.file.path;
+    let message = invalid ? 'Validation failed, entered data is incorrect.'
+        : !imageUrl ? 'No image provided.'
+        : 'Post updated successfully!';
+    if (invalid || !imageUrl) {
+        const error = new Error(message);
+        error.statusCode = 422;
+        throw error;
+    }
+    const {title, content} = req.body;
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(postId, {
+            title,
+            content,
+            imageUrl,
+        });
+        if (!updatedPost) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (imageUrl !== updatedPost.imageUrl) deleteFile(path.join(rootDir(), updatedPost.imageUrl));
+        res.status(200).json({message, post: updatedPost});
+    } catch (err) {
+        if (!err.statusCode) (err.statusCode = 500);
+        next(err);
+    }
+}
+
+const deletePost = async (req, res, next) => {
+    const {postId} = req.params;
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        // Check logged in user
+        
+        deleteFile(path.join(rootDir(), post.imageUrl));
+        const deletedPost = await Post.findByIdAndRemove(postId);
+        res.status(200).json({message: 'Deleted post.', post: deletedPost});
+    } catch (err) {
+        if (!err.statusCode) (err.statusCode = 500);
+        next(err);
+    }
+};
+
+export {getPosts, createPost, getPost, updatePost, deletePost};
--- Code/25_REST-APIs-The-Practical-Application/entities/feed/feed.routes.js
+++ Code/25_REST-APIs-The-Practical-Application/entities/feed/feed.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {createPost, getPost, getPosts} from './feed.controller.js';
+import {createPost, getPost, getPosts, updatePost, deletePost} from './feed.controller.js';
 import {postValidator} from "./feed.validator.js";
 
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/post', postValidator, createPost);
 router.get('/post/:postId', getPost);
 router.get('/posts', getPosts);
+router.put('/post/:postId', postValidator, updatePost);
+router.delete('/post/:postId', deletePost);
 
 export default router;
